3889e58dc36cd6aa1535367dcc95e3a4696e5cc1
sergey <sashumihin@gmail.com>
2021-03-30 18:37:15 +0300

    5 урок 1:08:35

---
+++ app/src/main/java/com/shumikhin/myfirsapp/Lesson5MainActivity.java
@@ -0,0 +1,14 @@
+package com.shumikhin.myfirsapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Lesson5MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.lesson5_activity_main);
+    }
+}
